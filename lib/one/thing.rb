0e67f9a3587bf1d400f8cd1fe7045193a569ebf2
Max Bigras <mbigras22@gmail.com>
2018-04-18 16:13:46 -0700

    Add documentation comments

--- lib/one/thing.rb
+++ lib/one/thing.rb
@@ -1,6 +1,26 @@
+##
+# hello world
+#   foo bar baz
+#   cats and dogs
 module One
+  ##
+  # hello world
+  #   foo bar baz
+  #   cats and dogs
   class Thing
+    ##
+    # hello world
+    #   foo bar baz
+    #   cats and dogs
     def call
+      "I'm a one!"
+    end
+
+    ##
+    # hello world
+    #   foo bar baz
+    #   cats and dogs
+    def flap(jack:, here: 42)
       "I'm a one!"
     end
   end
